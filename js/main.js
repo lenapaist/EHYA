2c0186b0e02a63f35bcea764ca7be77ca14cd502
lenapaist <lena.paist@gmail.com>
2021-10-13 21:16:03 +0300

    create: block form

--- js/main.js
+++ js/main.js
@@ -1,32 +1,91 @@
 $(document).ready(function () {
     const menuButton = $(".plate");
     const body = $("body");
-    const prevArrow = $(".slider__button--prev");
-    const nextArrow = $(".slider__button--next");
+    const prevArrow = $(".slider__button-prev");
+    const nextArrow = $(".slider__button-next");
+    const unpublishedPrev = $(".unpublished__button-prev");
+    const unpublishedNext = $(".unpublished__button-next");
     menuButton.on("click", function () {
         this.classList.toggle("active");
         $(".header__mobile").toggleClass("header__mobile--visible");
         $("body").toggleClass("hidden-scroll");
     });
-
-    $(".slide").slick({
+    $(".carousel").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        adaptiveHeight: true,
         prevArrow: prevArrow,
         nextArrow: nextArrow,
-        // responsive: [
-        //     {
-        //         breakpoint: 1200,
-        //         settings: {
-        //             slidesToShow: 3,
-        //         },
-        //     },
-        // ],
+        asNavFor: ".slide-adaptive",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 645,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
     });
-
-    var player;
+    $(".slide-adaptive").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        asNavFor: ".carousel",
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 645,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    });
+    $(".unpublished__books").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: false,
+        prevArrow: unpublishedPrev,
+        nextArrow: unpublishedNext,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1026,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 783,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+    let player;
     $(".video-play").on("click", function onYouTubeIframeAPIReady() {
         player = new YT.Player("player", {
             width: "100%",
@@ -36,6 +95,42 @@ $(document).ready(function () {
             },
         });
 
+        function videoPlay(event) {
+            event.target.playVideo();
+        }
+    });
+    $("#play1").on("click", function onYouTubeIframeAPIReady() {
+        player = new YT.Player("playerSmall", {
+            width: "100%",
+            videoId: "4EoUz39nPMM",
+            events: {
+                onReady: videoPlay,
+            },
+        });
+        function videoPlay(event) {
+            event.target.playVideo();
+        }
+    });
+    $("#play2").on("click", function onYouTubeIframeAPIReady() {
+        player = new YT.Player("playerSmall2", {
+            width: "100%",
+            videoId: "4EoUz39nPMM",
+            events: {
+                onReady: videoPlay,
+            },
+        });
+        function videoPlay(event) {
+            event.target.playVideo();
+        }
+    });
+    $("#play3").on("click", function onYouTubeIframeAPIReady() {
+        player = new YT.Player("playerSmall3", {
+            width: "100%",
+            videoId: "4EoUz39nPMM",
+            events: {
+                onReady: videoPlay,
+            },
+        });
         function videoPlay(event) {
             event.target.playVideo();
         }
